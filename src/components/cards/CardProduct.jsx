a237d631fef52247a814108df2fa1e8bd1d5ae01
Cristian Rodriguez <cristianrodrig149@gmail.com>
2023-01-12 00:43:27 +0100

    ✨feat: add  new components

---
+++ src/components/cards/CardProduct.jsx
@@ -0,0 +1,18 @@
+
+
+export const CardProduct = () => {
+  return (
+    <div>
+        <img src="" alt="" />
+        <p>
+            <span>Precio</span>
+        </p>
+        <p>
+            <span>Nombre Producto</span>
+        </p>
+        <button>
+            <span>Agregar al carrito</span>
+        </button>
+    </div>
+  )
+}
