88df23ea7eb7c508a0b811e921e2b8bcbe7717e5
Cristian Rodriguez <cristianrodrig149@gmail.com>
2023-01-12 00:42:08 +0100

    ✨feat: add button to close the menu and change  styles

--- src/components/header/ToggleMenu.jsx
+++ src/components/header/ToggleMenu.jsx
@@ -1,7 +1,9 @@
+import { PropTypes } from 'prop-types';
+
 import LogoMovil from '../../assets/logo mobile.png';
 import cerrar from '../../assets/equis-cerrar.png.png';
 
-export const ToggleMenu = () => {
+export const ToggleMenu = ({ cerrarToggleMenu }) => {
 	const navItems = [
 		{
 			title: 'Categorías',
@@ -18,30 +20,34 @@ export const ToggleMenu = () => {
 	];
 
 	return (
-		<div className='container mx-auto p-4 flex flex-col relative '>
+		<div className='container mx-auto p-4 flex flex-col relative h-screen '>
 			<div className='flex justify-between mb-4'>
 				<h2>Menu</h2>
-				<img src={cerrar} alt='cerrar' />
+				<button onClick={cerrarToggleMenu}>
+					<img src={cerrar} alt='cerrar' />
+				</button>
 			</div>
 
 			<hr />
 			<div>
-			<nav className='mt-5'>
-				<ul>
-					{navItems.map((item) => (
-						<li className='mb-4' key={item.title}>
-							<a href={item.url}>{item.title}</a>
-						</li>
-					))}
-				</ul>
-			</nav>
+				<nav className='mt-5'>
+					<ul>
+						{navItems.map((item) => (
+							<li className='mb-4' key={item.title}>
+								<a href={item.url}>{item.title}</a>
+							</li>
+						))}
+					</ul>
+				</nav>
 			</div>
 
-			<div className='absolute -bottom-full'>
+			<div className='h-2/5 flex items-end'>
 				<img src={LogoMovil} alt='Logo' />
 			</div>
-			
-			
 		</div>
 	);
+};
+
+ToggleMenu.propTypes = {
+	cerrarToggleMenu: PropTypes.func.isRequired,
 };
